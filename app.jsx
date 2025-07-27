c382454caaa76d5d85eaabdac52e83465bea08b4
Super Zombi <super.zombi.yt@gmail.com>
2025-07-27 18:59:46 +0300

    fix args

--- app.jsx
+++ app.jsx
@@ -1,7 +1,16 @@
 const App = () => {
 	return (
 		<div>
-			<Presence/>
+			<Presence actType="listening" state="State" details="Details"
+				large_image = "https://cdn.discordapp.com/embed/avatars/1.png"
+				small_image = "https://cdn.discordapp.com/app-assets/383226320970055681/1359299466493956258.png"
+				ts_start = {Math.floor(Date.now() / 1000) - 100}
+				ts_end = {Math.floor(Date.now() / 1000) + 100}
+				buttons = {[
+					{"label": "Ask to join", "url": "https://www.google.com/"},
+					{"label": "Ask to join", "url": "https://www.google.com/"},
+				]}
+			/>
 		</div>
 	)
 }
--- components/Presence.jsx
+++ components/Presence.jsx
@@ -1,17 +1,13 @@
 const Presence = ({
-	// actType = "playing",
-	actType = "listening",
+	actType = "playing",
 	appName = "AppName",
-	state="State",
-	details="Details",
-	large_image = "https://cdn.discordapp.com/embed/avatars/1.png",
-	small_image = "https://cdn.discordapp.com/app-assets/383226320970055681/1359299466493956258.png",
-	ts_start = Math.floor(Date.now() / 1000) - 100,
-	ts_end = Math.floor(Date.now() / 1000) + 100,
-	buttons = [
-		{"label": "Ask to join", "url": "https://www.google.com/"},
-		{"label": "Ask to join", "url": "https://www.google.com/"},
-	]
+	state="",
+	details="",
+	large_image="https://cdn.discordapp.com/embed/avatars/1.png",
+	small_image="",
+	ts_start=Math.floor(Date.now() / 1000),
+	ts_end="",
+	buttons=[]
 }) => {
 	return (
 		<div className="
@@ -20,7 +16,7 @@ const Presence = ({
 			rounded-xl
 			bg-[#3f4048] text-[#dfe0e2]
 		">
-			<Header actType={actType} appName={appName}/>
+			<Header actType={actType} appName={appName} details={details}/>
 			<Body
 				large_image={large_image} small_image={small_image}
 				actType={actType} appName={appName}
@@ -33,14 +29,17 @@ const Presence = ({
 }
 
 const Header = ({
-	actType, appName,
+	actType, appName, details
 }) => {
 	function renderAction(){
 		if (actType === "playing"){
 			return "Playing"
 		}
 		else if (actType === "listening"){
-			return `Listening to ${appName}`
+			return details ? `Listening to ${appName}` : "Listening to"
+		}
+		else if (actType === "watching"){
+			return details ? `Watching ${appName}` : "Watching"
 		}
 	}
 	return (
@@ -101,10 +100,10 @@ const RightBody = ({
 			<ActivityInfo actType={actType} appName={appName}
 				state={state} details={details}
 			/>
-			{["listening", "watching"].includes(actType) ? (
+			{(["listening", "watching"].includes(actType) && ts_end) ? (
 				<Progressbar ts_start={ts_start} ts_end={ts_end}/>
 			) : (
-				<Timer ts_start={ts_start}/>
+				<Timer ts_start={ts_start} actType={actType}/>
 			)}
 		</div>
 	)
@@ -115,12 +114,15 @@ const ActivityInfo = ({
 }) => {
 	return (
 		<div className="flex flex-col">
-			{["listening", "watching"].includes(actType) ? null : (
+			{(["listening", "watching"].includes(actType) && details) ? null : (
 				<div className="font-bold">{appName}</div>
 			)}
-			{details ? (
+			{(details && ["listening", "watching"].includes(actType)) ? 
+			(
+				<div className="font-bold">{details}</div>
+			) : (
 				<div className="text-sm">{details}</div>
-			) : null}
+			)}
 			{state ? (
 				<div className="text-sm">{state}</div>
 			) : null}
@@ -200,7 +202,7 @@ const Progressbar = ({
 }
 
 const Timer = ({
-	ts_start
+	ts_start, actType
 }) => {
 	const unixTime = Math.floor(Date.now() / 1000);
 	const [seconds, setSeconds] = React.useState(unixTime - ts_start);
@@ -214,9 +216,21 @@ const Timer = ({
 
 	return (
 		<div className="flex items-center gap-1">
-			<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
-				<path fill="#75c383" fillRule="evenodd" clipRule="evenodd" d="M20.97 4.06c0 .18.08.35.24.43.55.28.9.82 1.04 1.42.3 1.24.75 3.7.75 7.09v4.91a3.09 3.09 0 0 1-5.85 1.38l-1.76-3.51a1.09 1.09 0 0 0-1.23-.55c-.57.13-1.36.27-2.16.27s-1.6-.14-2.16-.27c-.49-.11-1 .1-1.23.55l-1.76 3.51A3.09 3.09 0 0 1 1 17.91V13c0-3.38.46-5.85.75-7.1.15-.6.49-1.13 1.04-1.4a.47.47 0 0 0 .24-.44c0-.7.48-1.32 1.2-1.47l2.93-.62c.5-.1 1 .06 1.36.4.35.34.78.71 1.28.68a42.4 42.4 0 0 1 4.4 0c.5.03.93-.34 1.28-.69.35-.33.86-.5 1.36-.39l2.94.62c.7.15 1.19.78 1.19 1.47ZM20 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM15.5 12a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 7a1 1 0 0 1 2 0v1h1a1 1 0 0 1 0 2H7v1a1 1 0 1 1-2 0v-1H4a1 1 0 1 1 0-2h1V7Z"/>
-			</svg>
+			{
+				actType === "listening" ? (
+					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+						<path fill="#75c383" d="M8.65 1.51A2 2 0 0 0 6 3.41v9.88A3.98 3.98 0 0 0 4.5 13C2.57 13 1 14.34 1 16s1.57 3 3.5 3S8 17.66 8 16V5.4l11 3.81v7.08a3.98 3.98 0 0 0-1.5-.29c-1.93 0-3.5 1.34-3.5 3s1.57 3 3.5 3 3.5-1.34 3.5-3V7.03c0-.74-.47-1.4-1.18-1.65L8.65 1.51Z"/>
+					</svg>
+				) : actType === "watching" ? (
+					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+						<path fill="#75c383" d="M4 3a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H4Zm2 17a1 1 0 1 0 0 2h12a1 1 0 1 0 0-2H6Z"/>
+					</svg>
+				) : (
+					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+						<path fill="#75c383" fillRule="evenodd" clipRule="evenodd" d="M20.97 4.06c0 .18.08.35.24.43.55.28.9.82 1.04 1.42.3 1.24.75 3.7.75 7.09v4.91a3.09 3.09 0 0 1-5.85 1.38l-1.76-3.51a1.09 1.09 0 0 0-1.23-.55c-.57.13-1.36.27-2.16.27s-1.6-.14-2.16-.27c-.49-.11-1 .1-1.23.55l-1.76 3.51A3.09 3.09 0 0 1 1 17.91V13c0-3.38.46-5.85.75-7.1.15-.6.49-1.13 1.04-1.4a.47.47 0 0 0 .24-.44c0-.7.48-1.32 1.2-1.47l2.93-.62c.5-.1 1 .06 1.36.4.35.34.78.71 1.28.68a42.4 42.4 0 0 1 4.4 0c.5.03.93-.34 1.28-.69.35-.33.86-.5 1.36-.39l2.94.62c.7.15 1.19.78 1.19 1.47ZM20 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM15.5 12a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 7a1 1 0 0 1 2 0v1h1a1 1 0 0 1 0 2H7v1a1 1 0 1 1-2 0v-1H4a1 1 0 1 1 0-2h1V7Z"/>
+					</svg>
+				)
+			}
 			<span className="font-mono font-bold text-xs text-[#75c383]">
 				{formatTime(seconds)}
 			</span>
