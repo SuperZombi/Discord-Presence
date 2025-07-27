8cc826355d90943622dc9f2a9cd85d5a8f2941e0
Super Zombi <super.zombi.yt@gmail.com>
2025-07-27 23:33:42 +0300

    loader

--- app.jsx
+++ app.jsx
@@ -1,6 +1,14 @@
 const App = () => {
+	const [showLoader, setShowLoader] = React.useState(true)
+	React.useEffect(() => {
+		setTimeout(_=>{
+			setShowLoader(false)
+		}, 3000)
+	}, [])
+
 	return (
 		<div>
+			<Loader show={showLoader}/>
 			<Presence actType="playing" state="State" details="Details"
 				large_image = "https://cdn.discordapp.com/embed/avatars/1.png"
 				small_image = "https://cdn.discordapp.com/app-assets/383226320970055681/1359299466493956258.png"
@@ -25,6 +33,29 @@ const App = () => {
 					{ value: "orange", label: "Orange" },
 				]} onChange={e=>console.log(e)}/>
 			</Card>
+		</div>
+	)
+}
+
+const Loader = ({show=true}) => {
+	const [shouldRender, setShouldRender] = React.useState(show)
+	React.useEffect(() => {
+		if (!show){
+			const timeout = setTimeout(() => setShouldRender(false), 200)
+			return () => clearTimeout(timeout);
+		} else {
+			setShouldRender(true)
+		}
+	}, [show])
+
+	return shouldRender && (
+		<div className={`
+			fixed inset-0 flex items-center justify-center
+			z-50 bg-[#323339]
+			duration-200 ease-out
+			${show ? "opacity-100" : "opacity-0"}
+		`}>
+			<video src="assets/loading.webm" autoPlay muted loop></video>
 		</div>
 	)
 }
