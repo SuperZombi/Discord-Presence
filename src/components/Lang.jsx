418cba7d2efba16d6f49a7898f01152378e62695
Super Zombi <super.zombi.yt@gmail.com>
2025-07-28 23:27:29 +0300

    update lang engine

--- src/components/Lang.jsx
+++ src/components/Lang.jsx
@@ -1,3 +1,4 @@
+const supported_langs = ["en", "ru"]
 var _LOCALE = {}
 const T = ({children, vars=null}) => {
 	return (
@@ -14,12 +15,22 @@ const Tt = (key, vars=null) => {
 const _LOCALE_MAP = (x, vars) => {
 	return Object.entries(vars || {}).reduce((s, [k, v]) => s.replaceAll(`{${k}}`, v), x)
 }
-async function init_lang(lang="en"){
+const init_lang = async (lang=null) => {
+	const userLang = lang || navigator.language.slice(0, 2).toLowerCase()
+	const lang_to_load = supported_langs.includes(userLang) ? userLang : "en"
+	if (lang_to_load !== userLang){
+		console.warn(`Language '${userLang}' is not supported!`)
+	}
 	try {
-		const response = await fetch(`/locales/${lang}.json`);
-		const data = await response.json();
+		const response = await fetch(`/locales/${lang_to_load}.json`)
+		const data = await response.json()
 		_LOCALE = data;
 	} catch (e) {
-		console.error(`Failed to load lang (${lang}):`, e);
+		console.error(`Failed to load lang '${lang_to_load}':\n\n`, e)
+		if (lang_to_load !== "en"){
+			await init_lang("en")
+		} else {
+			throw new Error("Failed to init language engine")
+		}
 	}
 }
