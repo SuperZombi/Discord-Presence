a99765513418dc212d2415db9ee7981ad8a72a0f
Super Zombi <super.zombi.yt@gmail.com>
2025-07-29 13:38:38 +0300

    react local

--- prepare-web.js
+++ prepare-web.js
@@ -9,5 +9,6 @@ async function prepareWebFolder() {
   await fs.copyFile(path.join(webDevDir, 'index.html'), path.join(webDir, 'index.html'));
   await fs.copy(path.join(webDevDir, 'assets'), path.join(webDir, 'assets'), { overwrite: true });
   await fs.copy(path.join(webDevDir, 'locales'), path.join(webDir, 'locales'), { overwrite: true });
+  await fs.copy(path.join(webDevDir, 'react'), path.join(webDir, 'react'), { overwrite: true });
 }
 prepareWebFolder().catch(console.error);
