e7ce4cc396cc367c41efb95b4c7ffc4255db8546
Super Zombi <super.zombi.yt@gmail.com>
2026-07-06 13:11:12 +0100

    add logo

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,20 @@
 const Home = () => {
 	return (
 		<>
-			<i className="fa-brands fa-discord"></i>
-			<h3>Hello world</h3>
-			<img className="h-32" src="images/picture.png"/>
+			<Section>
+				<i className="fa-brands fa-discord"></i>
+				<h3>Hello world</h3>
+				<img className="h-32" src="images/picture.png"/>
+			</Section>
 		</>
+	)
+}
+
+const Section = ({children}) => {
+	return (
+		<section className="p-4 min-h-[calc(100dvh-(--spacing(16)))]">
+			{children}
+		</section>
 	)
 }
 
@@ -15,9 +25,12 @@ const Header = () => {
 
 	return (
 		<header className="sticky top-0 bg-slate-800/50 backdrop-blur-md h-16">
-			<div className="max-w-6xl h-full m-auto px-8 py-2 flex items-center">
+			<div className="max-w-6xl h-full m-auto px-4 py-2 flex items-center">
 				<nav>
-					<Link to="/">Home</Link>
+					<Link to="/" className="flex items-center gap-3">
+						<img className="h-8" src="images/logo.png" alt="Logo" draggable={false}/>
+						<span className="text-lg font-semibold">Discord Presence</span>
+					</Link>
 				</nav>
 			</div>
 		</header>
@@ -36,11 +49,11 @@ const App = () => {
 		<div className="bg-slate-900 min-h-dvh text-gray-100">
 			<HashRouter>
 				<Header/>
-				<div className="max-w-6xl m-auto">
+				<main className="max-w-6xl m-auto">
 					<Switch>
 						<Route exact path="/" component={Home} />
 					</Switch>
-				</div>
+				</main>
 			</HashRouter>
 		</div>
 	)
