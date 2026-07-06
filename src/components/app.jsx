5c2bbe448982be83a48da9a497b9e3288744f8b3
Super Zombi <super.zombi.yt@gmail.com>
2026-07-06 12:20:07 +0100

    add react router

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,55 @@
-const App = () => {
+const {
+	useState,
+	useEffect
+} = React;
+
+const {
+	HashRouter,
+	Switch,
+	Route,
+	Link,
+	useHistory,
+	useLocation,
+	useParams
+} = ReactRouterDOM;
+
+const Home = () => {
 	return (
-		<React.Fragment>
+		<>
 			<i className="fa-brands fa-discord"></i>
 			<h3>Hello world</h3>
 			<img className="h-32" src="images/picture.png"/>
-		</React.Fragment>
+		</>
 	)
 }
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+const Test = () => {
+	const history = useHistory()
+
+	return (
+		<div>
+			<h2>Test</h2>
+			<button onClick={() => history.push("/")}>
+				Go home
+			</button>
+		</div>
+	)
+}
+
+const App = () => {
+	return (
+		<HashRouter>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/test">Test</Link>
+			</nav>
+
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/test" component={Test} />
+			</Switch>
+		</HashRouter>
+	)
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
