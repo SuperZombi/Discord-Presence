8360f1cdbfcd47adcffeaaeeb0834c1ed7083741
Super Zombi <super.zombi.yt@gmail.com>
2026-07-06 10:38:19 +0100

    Init Website

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<React.Fragment>
+			<i className="fa-brands fa-discord"></i>
+			<h3>Hello world</h3>
+			<img className="h-32" src="images/picture.png"/>
+		</React.Fragment>
+	)
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
