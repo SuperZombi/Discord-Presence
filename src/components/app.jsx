5a6e4e62e090d0de3193393194660fdfb991caff
Super Zombi <super.zombi.yt@gmail.com>
2026-07-07 17:58:28 +0100

    add sign

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -121,7 +121,13 @@ const Header = () => {
 	} = ReactRouterDOM;
 
 	return (
+		<>
+		<div className="bg-red-900/50 py-1 flex gap-2 items-center justify-center">
+			<i className="fa-solid fa-road-barrier text-amber-300"></i>
+			<span>This website is still under construction</span>
+		</div>
 		<header className="sticky top-0 z-50 h-16 border-b border-white/10 bg-slate-950/55 backdrop-blur-xl">
+			
 			<div className="max-w-6xl h-full m-auto px-4 py-2 flex items-center justify-between">
 				<nav>
 					<Link to="/" className="flex items-center gap-3">
@@ -132,6 +138,7 @@ const Header = () => {
 				<a href="#setup" className="hidden rounded-full border border-white/10 bg-white/10 px-4 py-2 text-sm font-semibold text-slate-100 transition hover:bg-white/15 sm:block">Начать</a>
 			</div>
 		</header>
+		</>
 	)
 }
 
